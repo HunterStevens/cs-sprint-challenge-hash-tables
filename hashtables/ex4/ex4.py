16e6845cb0389e3b65eb6ac4435bada2a347e10d
Hunter Stevens <hunterstevens1996@gmail.com>
2020-08-07 10:37:14 -0600

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,24 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    cahce_neg = {}
+    cahce_pos = {}
 
+    for i in a:
+        if i < 0:
+            if i in cahce_neg:
+                pass
+            else:
+                cahce_neg[i] = -i
+        elif i > 0:
+            if i in cahce_pos:
+                pass
+            else:
+                cahce_pos[i] = i
+    for i in cahce_neg:
+        if cahce_neg[i] in cahce_pos:
+            result.append(cahce_neg[i])
     return result
 
 
