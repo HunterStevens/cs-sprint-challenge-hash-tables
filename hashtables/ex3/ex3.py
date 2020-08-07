dbaf82c06ac3a08478e60ec6915874940609b23a
Hunter Stevens <hunterstevens1996@gmail.com>
2020-08-07 09:42:47 -0600

    emplete ex2 and ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,7 +3,28 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
-
+    result = []
+    intersect = {}
+    num_of_arr = len(arrays)
+    # print(num_of_arr)
+    for i in range(0, len(arrays)):
+        # print(i)
+        # print("\n")
+        for j in arrays[i]:
+            if j in intersect:
+                intersect[j] += 1
+                # print(j)
+                # print(intersect[j])
+                # print("\n")
+            else:
+                intersect[j] = 1
+    
+    for i in intersect:
+        # print(len(intersect[i]))
+        if intersect[i] == num_of_arr:
+            result.append(i)
+            
+    # print(result)
     return result
 
 
